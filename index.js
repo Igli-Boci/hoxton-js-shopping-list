4692511192ae2734f78ea21b3895be7012ece0c8
Igli-Boci <igliboci10@gmai.com>
2022-06-16 08:54:52 +0200

    Adding shopping list

---
+++ index.js
@@ -0,0 +1,8 @@
+let productsList = [
+    { name: 'Milk', price: 1.2 },
+    { name: 'Cocoa', price: 2 },
+    { name: 'Salad', price: 2 },
+    { name: 'Carrots', price: 2 },
+    { name: 'Tomatoes', price: 2.5 },
+    { name: 'Ready meals', price: 5 },
+];
