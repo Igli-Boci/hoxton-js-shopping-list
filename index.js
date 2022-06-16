947f5e476b9f79911664d6bb4ccefb7916296ab8
Igli-Boci <igliboci10@gmai.com>
2022-06-16 09:08:15 +0200

    Adding js to add a new product and its price and give us the total of the shopping list as well

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ let productsList = [
     { name: 'Tomatoes', price: 2.5 },
     { name: 'Ready meals', price: 5 },
 ];
+
+let addProduct = prompt('What else do you want to buy?');
+let productPrice = Number(prompt('Insert the new products price.'));
+
+let newProducts = { name: addProduct, price: productPrice };
+productsList.push(newProducts);
+
+let totalPrice = 0;
+for (let item of productsList) {
+    totalPrice += item.price;
+    console.log(item.name + ' costs ' + item.price + ' Eu.');
+}
+
+console.log('Total price: ' + totalPrice + ' Eu.');
